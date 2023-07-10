7403c32601e48456e1c4bdafea96cda38ac1cbe0
dilshad <dilshadbasith01@gmail.com>
2023-07-10 16:21:13 +0530

    first commit

---
+++ src/components/Comp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Comp.css";
+
+export default class Comp extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='heading'>A Tree's Tale of Connection and Conservation</h1>
+      </div>
+    )
+  }
+}
---
+++ src/components/Comp2.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./Comp.css";
+import img1 from "../assets/tree.png"
+
+
+export default class Comp2 extends Component {
+  render() {
+    return (
+      <div>
+      <div className="firstdiv">
+      <p className="para">
+          Once upon a time, in a lush green forest, there stood a mighty tree.
+          This tree was known as Oakheart, a majestic oak tree that had stood
+          tall for centuries. Its roots reached deep into the earth, drawing
+          nourishment and strength from the soil. Oakheart had witnessed the
+          passing of seasons, from the vibrant blossoms of spring to the golden
+          hues of autumn. It provided shelter and shade to various creatures of
+          the forest, including birds, squirrels, and insects. Its sturdy
+          branches swayed gently in the breeze, creating a soothing melody that
+          whispered through the forest. As the years went by, Oakheart saw
+          generations of animals seeking refuge within its branches. Birds built
+          their nests, squirrels stored their acorns, and deer rested under its
+          shade. The tree became a symbol of stability and wisdom in the
+          ever-changing forest. One day, a young girl named Lily ventured into
+          the forest. She had a deep love for nature and felt drawn to the
+          ancient tree. Intrigued by its grandeur, she sat down beneath its
+          branches and marveled at its magnificence. Lily spent hours near
+          Oakheart, observing the intricate patterns of its bark and the gentle
+          rustling of its leaves. She felt a sense of calm and serenity in its
+          presence, as if the tree was silently sharing its wisdom and stories
+          with her. As the seasons changed, Lily returned to the tree regularly,
+          nurturing a bond that grew stronger with each visit. She shared her
+          dreams and aspirations, and in return, Oakheart seemed to listen and
+          offer silent encouragement. Years passed, and Lily grew into a wise
+          and compassionate woman. She dedicated her life to environmental
+          conservation, spreading awareness about the importance of preserving
+          nature's beauty. She became a guardian of the forest, inspired by her
+          connection with Oakheart. In honor of her bond with the tree, Lily
+          organized a community event in the forest, inviting people from far
+          and wide to celebrate nature's wonders. Under the shelter of
+          Oakheart's branches, they planted saplings, symbolizing their
+          commitment to the earth and its protection. Oakheart stood tall and
+          proud, witnessing the gathering and the positive energy that emanated
+          from the event. It felt a deep sense of gratitude for Lily's love and
+          care, knowing that its purpose extended beyond providing shade and
+          shelter. Generations passed, and Oakheart remained a cherished
+          landmark in the forest. Its story was shared among families, and
+          children grew up hearing tales of the mighty oak and the girl who had
+          forged a profound connection with it. Even to this day, Oakheart
+          continues to thrive, standing as a testament to the enduring power of
+          nature and the profound impact a single tree can have on the lives of
+          those who appreciate its beauty and embrace its wisdom.
+        </p>
+        <img src={img1} alt="not found" />
+      </div>      
+      </div>
+    );
+  }
+}
